e21045fa2ad882ecc92100370c27d4af830fa984
Arham Shahzad <arhamawan715@gmail.com>
2025-12-24 16:21:05 +0500

    variables basics practice (Day 1)
    This file covers Python variables basics including:
    - variable declaration
    - data types
    - simple examples for beginners

---
+++ variables.py
@@ -0,0 +1,21 @@
+# variables.py
+# This file explains how variables work in Python
+
+# A variable stores data in memory
+name = "Arham"        # string type
+age = 18              # integer type
+height = 5.8          # float type
+is_student = True     # boolean type
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Variables can be updated
+age = age + 1
+print("Updated Age:", age)
+
+# Python is dynamically typed
+# This means we do not need to define the data type explicitly
