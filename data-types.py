66cdf2d08531de4f35d169dbc19b945550557224
Arham Shahzad <arhamawan715@gmail.com>
2025-12-24 16:28:20 +0500

    data types basics practice (Day 2)
    This commit covers Python data types basics including:
    - int, float, string, boolean
    - type() 
    -List,Tuple

---
+++ data-types.py
@@ -0,0 +1,27 @@
+# data_types.py
+# This file demonstrates common Python data types
+
+# Integer
+marks = 95
+print("Marks:", marks, type(marks))
+
+# Float
+percentage = 89.5
+print("Percentage:", percentage, type(percentage))
+
+# String
+message = "Learning Python"
+print("Message:", message, type(message))
+
+# Boolean
+is_passed = True
+print("Passed:", is_passed, type(is_passed))
+
+# List (collection of multiple values)
+numbers = [1, 2, 3, 4, 5]
+print("Numbers List:", numbers, type(numbers))
+
+# Tuple (immutable list)
+coordinates = (10, 20)
+print("Coordinates:", coordinates, type(coordinates))
+
